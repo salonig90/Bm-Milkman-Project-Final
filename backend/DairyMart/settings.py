e0cb3642d5f5fcbe92f22abc187dfa25ab48edfb
Ubuntu <salonig@saloni-project-vm.b5tdr43fznrelgjruuyifkdgfd.px.internal.cloudapp.net>
2026-03-19 10:32:26 +0000

    trigger deployment

--- backend/DairyMart/settings.py
+++ backend/DairyMart/settings.py
@@ -88,6 +88,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     proxy: {
       '/api': {
@@ -13,4 +14,10 @@ export default defineConfig({
       },
     },
   },
+
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: ['dairymart.duckdns.org']
+  }
 })
